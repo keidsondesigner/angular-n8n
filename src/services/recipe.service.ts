ec5b664335ea8bc4435733f5f42b75446fe48a50
Keidson Roby <keidsondesigner@gmail.com>
2025-05-10 01:10:40 -0400

    fix: production endpoint

--- src/services/recipe.service.ts
+++ src/services/recipe.service.ts
@@ -13,7 +13,7 @@ export class RecipeService {
   // private webhookUrl = 'https://keidsonroby.app.n8n.cloud/webhook-test/f4bac492-fbf1-4d9e-a41b-0f28d2fcdfdc';
 
   // url de produção
-  private webhookUrl = 'https://keidsonroby.app.n8n.cloud/webhook/f4bac492-fbf1-4d9e-a41b-0f28d2fcdfdc';
+  private webhookUrl = 'https://n8n-w8co.onrender.com/webhook/f4bac492-fbf1-4d9e-a41b-0f28d2fcdfdc';
 
   constructor(private http: HttpClient) {}
 
